678fb5e57b6cd9884ec60ae7541214a50f28f3c9
FatED42 <cjkrol42@gmail.com>
2019-12-08 21:16:35 +0300

    Дз к 4 лекции (с инфо о городе на вики)

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,46 +2,79 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.TextView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox checkBoxAir;
     private CheckBox checkBoxCloud;
-    private TextView airHumidity;
-    private TextView overcast;
+    Spinner citiesSpinner;
+    Button showWeatherBtn;
+
+    final static String KEY_TO_DATA = "KEY_TO_DATA";
+    private final static String CITY_STATE = "cityState";
+    private final static String HUMIDITY_STATE = "humidityState";
+    private final static String OVERCAST_STATE = "overcastState";
+
+    private final String[] cities = {"Москва", "Санкт-Петербург", "Зеленоград", "Рославль"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
 
-        checkBoxAir = (CheckBox)findViewById(R.id.checkBoxAir);
-        checkBoxCloud = (CheckBox)findViewById(R.id.checkBoxCloud);
-        airHumidity = (TextView)findViewById(R.id.textView);
-        overcast = (TextView)findViewById(R.id.textView2);
+    public void init() {
+        checkBoxAir = findViewById(R.id.checkBoxAir);
+        checkBoxCloud = findViewById(R.id.checkBoxCloud);
+        citiesSpinner = findViewById(R.id.citiesSpinner);
+        showWeatherBtn = findViewById(R.id.showWeather);
 
-        checkBoxAir.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.preview_text,
+                cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        citiesSpinner.setAdapter(adapter);
+
+        showWeatherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (checkBoxAir.isChecked()) {
-                    airHumidity.setVisibility(View.VISIBLE);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                ActivityInfo activityInfo = intent.resolveActivityInfo(getPackageManager(),
+                        intent.getFlags());
+                if (activityInfo != null) {
+                    Parcel parcel = new Parcel();
+                    parcel.city = citiesSpinner.getSelectedItem().toString();
+                    parcel.humidity = checkBoxAir.isChecked();
+                    parcel.overcast = checkBoxCloud.isChecked();
+                    intent.putExtra(KEY_TO_DATA, parcel);
+                    startActivity(intent);
                 }
-                else airHumidity.setVisibility(View.INVISIBLE);
             }
         });
-        checkBoxCloud.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (checkBoxCloud.isChecked()) {
-                    overcast.setVisibility(View.VISIBLE);
-                }
-                else overcast.setVisibility(View.INVISIBLE);
-            }
-        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        citiesSpinner.getSelectedItemPosition();
+        outState.putInt(CITY_STATE, citiesSpinner.getSelectedItemPosition());
+        outState.putBoolean(HUMIDITY_STATE, checkBoxAir.isChecked());
+        outState.putBoolean(OVERCAST_STATE, checkBoxCloud.isChecked());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        citiesSpinner.setSelection(savedInstanceState.getInt(CITY_STATE));
+        checkBoxAir.setChecked(savedInstanceState.getBoolean(HUMIDITY_STATE));
+        checkBoxCloud.setChecked(savedInstanceState.getBoolean(OVERCAST_STATE));
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/Parcel.java
@@ -0,0 +1,9 @@
+package com.example.weatherapp;
+
+import java.io.Serializable;
+
+public class Parcel implements Serializable {
+    String city = "";
+    Boolean humidity = false;
+    Boolean overcast = false;
+}
